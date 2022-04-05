0720ea5e5f3d6ef48e83ef2941474c26e19491f8
MSCxar0293 <mscxprivatemail@163.com>
2022-04-05 23:09:19 +0800

    新插件：狗屁不通文章生成器

---
+++ gpbt200/gpbt200.go
@@ -0,0 +1,36 @@
+//Package gpbt200 狗屁不通文章生成器 调用api版
+package gpbt200
+
+import ( 
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+
+	control "github.com/FloatTech/zbputils/control"
+	zero "github.com/wdvxdr1123/ZeroBot"
+	"github.com/wdvxdr1123/ZeroBot/message"
+)
+
+func init(){
+	engine := control.Register("gpbt200", &control.Options{
+		DisableOnDefault: false,
+		Help: "gpbt200\n" +
+		"- 狗屁不通#关键字\n" +
+		"例：狗屁不通#喝酸奶",
+	})
+	type gpbt struct {
+		CODE int `json:"code"`
+		TEXT string `json:"text"`
+	}
+	engine.OnRegex ("狗屁不通#(.*)").SetBlock(true).
+	Handle(func(ctx *zero.Ctx){
+		keyw := ctx.State["regex_matched"].([]string)[1]
+		url := "http://ovooa.com/API/dog/api.php?msg="+ keyw +"&num=200&type=json"
+		resp, _ := http.Get(url)
+		s := gpbt{}
+		body, _ := ioutil.ReadAll(resp.Body)
+		resp.Body.Close()
+		json.Unmarshal([]byte(body), &s)
+		ctx.SendChain(message.Text(s.TEXT))
+	})
+}
