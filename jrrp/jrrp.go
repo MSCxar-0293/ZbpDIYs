e1aa581b850e30579add01155a043370b6984d49
MSCxar0293 <mscxprivatemail@163.com>
2022-05-09 00:43:41 +0800

    jrrp修复：修复由于获取时间未放在触发关键词后执行的代码块下导致的「不重启就一直一个结果」的⑨问题

--- jrrp/jrrp.go
+++ jrrp/jrrp.go
@@ -17,25 +17,23 @@ func init(){
 		Help: "jrrp\n" +
 		"- 今日人品",
 	})
-
-			now := time.Now().Format("20060102")
 			var signTF map[string](int)
 			signTF = make(map[string](int))
-			var result map[int64](int)
-			result =  make(map[int64](int))
+			var result map[string](int)
+			result =  make(map[string](int))
 	engine.OnFullMatch ("今日人品").SetBlock(true).
 	Handle(func(ctx *zero.Ctx){
+		        now := time.Now().Format("20060102")
 			user := ctx.Event.UserID
 			userS := strconv.FormatInt(user,10)
 			var si string = now + userS
-			rand.Seed(time.Now().UnixNano())
-			today := rand.Intn(100)
 		if signTF [si] == 0 {
 			signTF [si] = (1)
-			result [user] = (today)
-		ctx.SendChain(message.At(user),message.Text(" 阁下今日的人品值为",result [user],"呢~\n"),message.Image("https://img.qwq.nz/images/2022/04/04/aab2985d94e996558b303be42a954a4f.jpg"))
+			today := rand.Intn(100)
+			result [si] = (today)
+		ctx.SendChain(message.At(user),message.Text(" 阁下今日的人品值为",result [si],"呢~\n"),message.Image("https://img.qwq.nz/images/2022/04/04/aab2985d94e996558b303be42a954a4f.jpg"))
 		} else {
-			ctx.SendChain(message.At(user),message.Text(" 阁下今日的人品值为",result [user],"呢~\n"),message.Image("https://img.qwq.nz/images/2022/04/04/aab2985d94e996558b303be42a954a4f.jpg"))
+			ctx.SendChain(message.At(user),message.Text(" 阁下今日的人品值为",result [si],"呢~\n"),message.Image("https://img.qwq.nz/images/2022/04/04/aab2985d94e996558b303be42a954a4f.jpg"))
 		}
 
 	})
